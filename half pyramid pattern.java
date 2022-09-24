6710c2fa182700b1cb44b6be8af8fc1cf954a5cc
Abisha Muthukrishnan <110807012+Abida-M@users.noreply.github.com>
2022-09-24 08:37:04 +0530

    Create half pyramid pattern.java

---
+++ half pyramid pattern.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class Main
+{
+	public static void main(String[] args)throws IOException
+	{
+		int r,i,j;
+		DataInputStream d=new DataInputStream(System.in);
+		System.out.print("Enter the number of rows:");
+		r=Integer.parseInt(d.readLine());
+		for(i=1;i<=r;i++)
+		{
+		    for(j=1;j>=1 && j<=i;j++)
+		        System.out.print("* ");
+		System.out.print("\n");
+		}
+	}
+}
