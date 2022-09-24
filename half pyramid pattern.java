a29c247e7ce5965dd93710b7fd3e7ad7e6068b62
Abisha Muthukrishnan <110807012+Abida-M@users.noreply.github.com>
2022-09-24 09:02:16 +0530

    Update half pyramid pattern.java

--- half pyramid pattern.java
+++ half pyramid pattern.java
@@ -9,7 +9,7 @@ public class Main
 		r=Integer.parseInt(d.readLine());
 		for(i=1;i<=r;i++)
 		{
-		    for(j=1;j>=1 && j<=i;j++)
+		    for(j=1;j<=i;j++)
 		        System.out.print("* ");
 		System.out.print("\n");
 		}
